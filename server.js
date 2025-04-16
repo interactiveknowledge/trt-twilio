aeeb05713ad07854b9fdb61642806ae872ed2a5d
Philip Curley <philip@interactiveknowledge.com>
2025-04-16 12:12:15 -0400

    redis

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express')
 const { MessagingResponse } = require('twilio').twiml
 const bodyParser = require('body-parser')
 const { hasValidZipCode, parseZipCode } = require('./utilities.js')
+const redis = require('redis')
+const redisUrl = process.env.REDISCLOUD_URL || 'redis://localhost:6379'
+const client = redis.createClient(redisUrl)
 
 // Handler for incoming messages.
 const incomingMessageHandler = (req, res) => {
   // Get message body.
   const messageBody = req.body.Body
   const twiml = new MessagingResponse()
+  const from = req.body.From
 
   if (messageBody === 'LOCATE') {
     twiml.message('We can do that! This is The Right Time clinic finder. Please send your zip code to find a clinic near you.')
+  }
+  else if (messageBody = 'STATS') {
+    client.get(from, (err, value) => {
+      twiml.message(`You have sent ${parseInt(value) + 1} messages to us.`)
+    })
   }
   else if (hasValidZipCode(messageBody) === true) {
     const zipCode = parseZipCode(messageBody)
@@ -18,6 +27,17 @@ const incomingMessageHandler = (req, res) => {
   }
 
   res.type('text/xml').send(twiml.toString())
+
+  // Increment the message count for the user.
+  client.get(from, (err, value) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    const count = parseInt(value) + 1
+    client.set(from, count)
+  })
 }
 
 // Set up server.
